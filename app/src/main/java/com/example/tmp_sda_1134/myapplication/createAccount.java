5449b14074442858f117023c2ae4a556f55da6a4
tmp-sda-1134 <soni.kashish@gmail.com>
2017-11-07 15:09:40 +0100

    2nd com mit

---
+++ app/src/main/java/com/example/tmp_sda_1134/myapplication/createAccount.java
@@ -0,0 +1,18 @@
+package com.example.tmp_sda_1134.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tmp-sda-1134 on 11/7/17.
+ */
+
+
+public class createAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
--- app/src/main/java/com/example/tmp_sda_1134/myapplication/userDetails.java
+++ app/src/main/java/com/example/tmp_sda_1134/myapplication/userDetails.java
@@ -1,14 +1,17 @@
 package com.example.tmp_sda_1134.myapplication;
 
+/**
+ * Created by tmp-sda-1134 on 11/7/17.
+ */
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class userDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hiiiiiiiii");
     }
 }
