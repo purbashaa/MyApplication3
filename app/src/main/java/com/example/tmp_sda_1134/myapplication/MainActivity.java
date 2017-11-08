f10ab9043ea7c8a9be56b7e88ea7b62e679a4ec6
tmp-sda-1134 <soni.kashish@gmail.com>
2017-11-08 15:48:54 +0100

    3rd com mit

--- app/src/main/java/com/example/tmp_sda_1134/myapplication/MainActivity.java
+++ app/src/main/java/com/example/tmp_sda_1134/myapplication/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.tmp_sda_1134.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class
+MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        System.out.println("Hiiiiiiiii");
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+
+
+            final EditText etUserName = (EditText) findViewById(R.id.etUserName);
+            final EditText etpassword = (EditText) findViewById(R.id.etpassword);
+            final Button btLogin = (Button) findViewById(R.id.btLogin);
+            final Button registerlink = (Button) findViewById(R.id.btCreateNwAccnt);
+
+
+
+
+            registerlink.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent createAccountIntent = new Intent(MainActivity.this, createAccount.class);
+                    MainActivity.this.startActivity(createAccountIntent);
+                }
+            });
+
+        }
     }
-}
+
--- app/src/main/java/com/example/tmp_sda_1134/myapplication/createAccount.java
+++ app/src/main/java/com/example/tmp_sda_1134/myapplication/createAccount.java
@@ -2,6 +2,8 @@ package com.example.tmp_sda_1134.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by tmp-sda-1134 on 11/7/17.
@@ -14,5 +16,15 @@ public class createAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        final EditText etName = (EditText) findViewById(R.id.etName);
+        final EditText etAge = (EditText) findViewById(R.id.etAge);
+        final EditText etCity = (EditText) findViewById(R.id.etCity);
+        final EditText etHobby1 = (EditText) findViewById(R.id.etHobby1);
+        final EditText etHobby2 = (EditText) findViewById(R.id.etHobby2);
+        final EditText etUserid = (EditText) findViewById(R.id.etUserid);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final Button btRegister = (Button) findViewById(R.id.btRegister);
+        final Button btCancel = (Button) findViewById(R.id.btCancel);
     }
 }
